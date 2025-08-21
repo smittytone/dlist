6be5feb3ccb974be94bbe758f6e33e8b0df9f7c7
Tony Smith (@smittytone) <smittytone@icloud.com>
2025-08-21 17:42:06 +0100

    Add Clicore

--- Package.swift
+++ Package.swift
@@ -1,19 +1,26 @@
-// swift-tools-version: 5.4
+// swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "dlist",
+    dependencies: [
+        .package(url: "https://github.com/smittytone/clicore", branch: "main"),
+    ],
     targets: [
         .executableTarget(
             name: "dlist",
-            dependencies: ["Clibudev"],
+            //dependencies: [],
+            dependencies: [
+                "Clibudev",
+                .product(name: "Clicore", package: "clicore"),
+            ],
             path: "dlist",
             exclude: [
                 // File not needed for Linux build (so far...)
                 "Info.plist",
-                "mac_aliases.swift"    
+                "mac_aliases.swift"
             ]
         ),
         .systemLibrary(
