74e9161a8f1b0358f374e5b2dc18e60cb21d04fa
Tony Smith (smittytone) <tony@smittytone.net>
2026-09-16 09:20:38 +0100

    Bump to 0.4.1

--- dlist/swift_version.swift
+++ dlist/swift_version.swift
@@ -29,5 +29,5 @@
          source of truth for the app's version and build numbers.
 */
 
-let SWIFT_BUILD_PROCESS_DLIST_VERSION = "0.4.0"
-let SWIFT_BUILD_PROCESS_DLIST_BUILD = 45
+let SWIFT_BUILD_PROCESS_DLIST_VERSION = "0.4.1"
+let SWIFT_BUILD_PROCESS_DLIST_BUILD = 46
